c048f06209484f7d5adf474bcbc5aafc585eefc3
HidalgoJoni <jonigarciah@gmail.com>
2024-02-02 13:06:38 +0100

    Update ConsultaActivity.java
    Creada la actividad de consulta, con un set on click listener para el boton de buscar, que verifica que el elemento que buscamos existe y que nos muestra en unos textviews debajo su contenido en la base de datos
    
    Tambien tenemos un set on click para el boton de volver que nos devuelve a la pantalla principal

--- app/src/main/java/com/example/examen2ev/ConsultaActivity.java
+++ app/src/main/java/com/example/examen2ev/ConsultaActivity.java
@@ -2,13 +2,82 @@ package com.example.examen2ev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConsultaActivity extends AppCompatActivity {
+    private TextView simbolo, numAtomico, estado;
+    private QuimicaSQLiteHelper dbQuimica;
+    private EditText nombre;
+    private Button btnBuscar, btnCancelar;
+
+    private int numConsultas = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta);
+
+        // encontramos los ids
+
+        btnBuscar = findViewById(R.id.btnBuscar);
+        btnCancelar = findViewById(R.id.btnCancelar);
+
+        nombre = findViewById(R.id.nombre);
+
+        simbolo = findViewById(R.id.simbolo);
+        numAtomico = findViewById(R.id.numAtomico);
+        estado = findViewById(R.id.estado);
+
+        dbQuimica = new QuimicaSQLiteHelper(this);
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ConsultaActivity.this, MainActivity.class);
+                i.putExtra("consultas", numConsultas);
+                finish();
+            }
+        });
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (dbQuimica.existeElemento(nombre.getText().toString())) {
+                    Elemento elemento = dbQuimica.obtenerElemento(nombre.getText().toString());
+
+                    simbolo.setText(elemento.getSimbolo());
+                    numAtomico.setText(elemento.getNumAtomico());
+                    estado.setText(elemento.getEstado());
+
+                    simbolo.setVisibility(View.VISIBLE);
+                    numAtomico.setVisibility(View.VISIBLE);
+                    estado.setVisibility(View.VISIBLE);
+
+                    numConsultas++;
+
+                    btnBuscar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            nombre.setText("");
+                            simbolo.setText("");
+                            numAtomico.setText("");
+                            estado.setText("");
+
+                            simbolo.setVisibility(View.INVISIBLE);
+                            numAtomico.setVisibility(View.INVISIBLE);
+                            estado.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                } else {
+                    Toast.makeText(ConsultaActivity.this, "El elemento no existe", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
