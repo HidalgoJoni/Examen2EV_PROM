43e0e553b909acdba2313f36873b241ac837f9a3
HidalgoJoni <jonigarciah@gmail.com>
2024-02-02 10:36:12 +0100

    UPDATE MainActivity acabado (de momento)

--- app/src/main/java/com/example/examen2ev/MainActivity.java
+++ app/src/main/java/com/example/examen2ev/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.examen2ev;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnQuimico, btnConsultar, btnSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // encontramos los ids de los botones
+
+        btnConsultar = findViewById(R.id.btnConsultar);
+        btnQuimico = findViewById(R.id.btnQuimico);
+        btnSalir = findViewById(R.id.btnSalir);
+
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ConsultaActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnQuimico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AdministracionActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("INFORMACIÓN")
+                        .setMessage("Esperamos verle pronto de nuevo.")
+                        .setPositiveButton("Cerrar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+
+                        })
+                        .show();
+            }
+        });
     }
 }
