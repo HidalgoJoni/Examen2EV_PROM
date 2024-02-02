dea07d9b88a7d1c4579367c56406b19567668f9e
HidalgoJoni <jonigarciah@gmail.com>
2024-02-02 10:27:37 +0100

    Create Elemento.java
    Creado el modelo que vamos a usar para cada elemento

---
+++ app/src/main/java/com/example/examen2ev/Elemento.java
@@ -0,0 +1,57 @@
+package com.example.examen2ev;
+
+public class Elemento {
+    private int identificacion;
+    private String nombre;
+    private String simbolo;
+    private int numAtomico;
+    private String estado;
+
+    public Elemento (int id, String nom, String simbolo, int num, String estado){
+        this.identificacion=id;
+        this.nombre=nom;
+        this.simbolo=simbolo;
+        this.numAtomico=num;
+        this.estado=estado;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public int getNumAtomico() {
+        return numAtomico;
+    }
+
+    public void setNumAtomico(int numAtomico) {
+        this.numAtomico = numAtomico;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
