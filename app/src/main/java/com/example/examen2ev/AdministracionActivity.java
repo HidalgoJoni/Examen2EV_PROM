8f7f5c36a28f57cb54df9050c43788e07200fc41
HidalgoJoni <jonigarciah@gmail.com>
2024-02-02 13:05:14 +0100

    Update AdministracionActivity.java
    Terminados los metodos de gestion de los botones de insertar, modificar, volver y eliminar en la actividad de administracion

--- app/src/main/java/com/example/examen2ev/AdministracionActivity.java
+++ app/src/main/java/com/example/examen2ev/AdministracionActivity.java
@@ -34,13 +34,34 @@ public class AdministracionActivity extends AppCompatActivity {
 
         dbQuimica = new QuimicaSQLiteHelper(this);
 
+        // Controlamos el set on click del boton de volver, que en su primera pulsacion actuara como si fuese un boton de reiniciar campos y botones, despues de su segunda pulsacion se saldra a la pantalla principal
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                nombre.setActivated(true);
+                simbolo.setActivated(true);
+                numAtomico.setActivated(true);
+                estado.setActivated(true);
+
+                nombre.setText("");
+                simbolo.setText("");
+                numAtomico.setText("");
+                estado.setText("");
+
+                btnInsertar.setEnabled(true);
+                btnModificar.setEnabled(true);
+                btnBorrar.setEnabled(true);
+
+                btnVolver.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
             }
         });
 
+        // Controlamos el set on click del boton de insertar, primero validamos los datos y que no exista el elemento en la tabla y despues procedemos a su insercion
         btnInsertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,21 +77,96 @@ public class AdministracionActivity extends AppCompatActivity {
             }
         });
 
+
+        // Controlamos el set on click del boton modificar, primero miramos si existe el elemento y despues le hacemos repetir los cambios que quiere hacer (como una verificacion de que de verdad quiere hacer eso), tambien desactivamos el resto de botones para que solo pueda actualizar o volver y procedemos con la actualizacion despues de verificar los datos
         btnModificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                if (dbQuimica.existeElemento(nombre.getText().toString())){
+                    nombre.setActivated(false);
+                    simbolo.setText("");
+                    numAtomico.setText("");
+                    estado.setText("");
+
+                    btnInsertar.setEnabled(false);
+                    btnBorrar.setEnabled(false);
+
+                    Toast.makeText(AdministracionActivity.this, "Introduce los cambios que necesites hacer en ese elemento", Toast.LENGTH_LONG).show();
+                    btnModificar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Elemento nuevoElemento = new Elemento(nombre.getText().toString(), simbolo.getText().toString() ,Integer.parseInt(numAtomico.getText().toString()), estado.getText().toString());
+                            if (validarDatos()){
+                                dbQuimica.actualizarElemento(nuevoElemento);
+                                Toast.makeText(AdministracionActivity.this, "Elemento actualizado", Toast.LENGTH_SHORT).show();
+
+                                nombre.setActivated(true);
+
+                                nombre.setText("");
+                                simbolo.setText("");
+                                numAtomico.setText("");
+                                estado.setText("");
+
+                                btnInsertar.setEnabled(true);
+                                btnBorrar.setEnabled(true);
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(AdministracionActivity.this, "No puedes modificar un elemento que todavia no esta en la tabla", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
+
+        // Controlamos el set on click del boton de borrar, practicamente casi igual al de actualizar, pero en vez de desactivar solo los botones desactivamos todo menos el nombre, tambien para que tenga que insertarlo dos veces como verificacion
         btnBorrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                if (dbQuimica.existeElemento(nombre.getText().toString())){
+                    nombre.setText("");
+
+                    // Desactivamos y dejamos vacios los campos y botones (excepto volver y borrar) para que vuelva a introducir el nombre del elemento que desea borrar
+                    numAtomico.setText("");
+                    numAtomico.setActivated(false);
+                    estado.setText("");
+                    estado.setActivated(false);
+                    simbolo.setText("");
+                    simbolo.setActivated(false);
+
+                    btnInsertar.setEnabled(false);
+                    btnModificar.setEnabled(false);
+
+                    Toast.makeText(AdministracionActivity.this, "Vuelve a introducir el nombre que deseas BORRAR de la tabla", Toast.LENGTH_LONG).show();
+                    btnBorrar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Elemento elementoABorrar = new Elemento(nombre.getText().toString(), simbolo.getText().toString() ,Integer.parseInt(numAtomico.getText().toString()), estado.getText().toString());
+                            dbQuimica.borrarElemento(elementoABorrar);
+                            Toast.makeText(AdministracionActivity.this, "Elemento eliminado correctamente", Toast.LENGTH_SHORT).show();
+
+                            simbolo.setActivated(true);
+                            numAtomico.setActivated(true);
+                            estado.setActivated(true);
+
+                            nombre.setText("");
+                            simbolo.setText("");
+                            numAtomico.setText("");
+                            estado.setText("");
+
+                            btnInsertar.setEnabled(true);
+                            btnModificar.setEnabled(true);
+                        }
+                    });
+                } else {
+                    Toast.makeText(AdministracionActivity.this, "No puedes borrar un elemento que todavia no esta en la tabla", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
+
+    // Validamos todos los datos antes de insertarlos o actualizarlos en la tabla
     boolean validarDatos(){
         String errores="";
 
