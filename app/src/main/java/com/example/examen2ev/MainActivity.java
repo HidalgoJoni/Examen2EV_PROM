9c0872585e5c4e0fbcceefe3257693905967f4a6
HidalgoJoni <jonigarciah@gmail.com>
2024-02-02 11:33:40 +0100

    Update MainActivity.java
    Cambio en la gestion del intent al hacer click en el boton quimico

--- app/src/main/java/com/example/examen2ev/MainActivity.java
+++ app/src/main/java/com/example/examen2ev/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         btnQuimico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, AdministracionActivity.class);
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
             }
         });
